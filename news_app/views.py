7780602d2d36d730bb477f2ccc56640f6f46bccc
Same272 <umarbekusmanovich@gmail.com>
2024-11-24 21:04:58 +0500

    trdgd

--- news_app/views.py
+++ news_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import News
 
+def home(request):
+    news_list = News.objects.all().order_by('-created_at')
+    return render(request, 'news/index.html', {'news_list': news_list})
 # Create your views here.
+def index(request):
+    return render(request, 'news_app/index.html')
