29d8367d755ebbf3cd16a3c7b04bc582e920a0a6
Same272 <umarbekusmanovich@gmail.com>
2024-12-02 21:41:00 +0500

    trdgd

--- newsas/settings.py
+++ newsas/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'newsas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'newss',
+        'OWNER': 'postgres',
+        'PORT': 5432,
+        'HOST': 'localhost',
+        'PASSWORD': 'admin'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
